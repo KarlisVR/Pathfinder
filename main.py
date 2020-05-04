04893b5575697dacc4fc60df132d0c5b253cb958
KarlisVR <rudevics.k@gmail.com>
2020-05-04 20:30:33 +0300

    Improved comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """ 
 	Pathfinder implements A* algorithm to find a path between two nodes.
-	User can choose the start and end node, and create obstacales
+	User can choose the start and end node, and create obstacles
 	between them.
 """
 
